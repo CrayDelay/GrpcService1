a5813a4577852f36383bc1bd512d6b1976a0c65a
CrayDelay <vitlana.belei@gmail.com>
2023-02-22 20:35:10 +0200

    Add project files.

---
+++ GrpcService1/AutoMapper/OfferMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using GrpcService1.Entities;
+using GrpcService1.Protos;
+
+namespace GrpcService1.AutoMapper
+{
+    public class OfferMapper : Profile
+    {
+        public OfferMapper()
+        {
+            CreateMap<Offer, OfferDetail>().ReverseMap();
+        }
+    }
+}
---
+++ GrpcService1/Data/DbContextClass.cs
@@ -0,0 +1,21 @@
+﻿using GrpcService1.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GrpcService1.Data
+{
+    public class DbContextClass : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DbContextClass(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public DbSet<Offer> Offer { get; set; }
+    }
+}
